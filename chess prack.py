e84cd9e06974b6bb35f7b7d7e569c9010c65d72f
hugh_mat <hugh.mathys@aad.gov.au>
2025-03-29 14:38:05 +1100

    udated chess game, two peices moving

--- chess prack.py
+++ chess prack.py
@@ -1,66 +1,50 @@
-import math
-import time
-
-class pieces:
+class Piece:
     def __init__(self, type, pos_x, pos_y):
         self.type = type
         self.position = (pos_x, pos_y)
         self.name = f"{self.type}"
 
 
-
-
-   
-
-
-
-
-
-
-
-
-def board(self, position):
-        self.position = position
-        for i in range(1, 9):
-            print(" ---" * 8)
-            for j in range(1, 9):
-                if (i, j) == self.position:
-                    print(f"| {self.type} ", end="")
-                else:
-                    print("|   ", end="")
-            print("|")
+def board(white_piece, black_piece):
+    for i in range(1, 9):
         print(" ---" * 8)
+        for j in range(1, 9):
+            if (i, j) == white_piece.position:
+                print(f"| {white_piece.type} ", end="")
+            elif (i, j) == black_piece.position:
+                print(f"| {black_piece.type} ", end="")
+            else:
+                print("|   ", end="")
+        print("|")
+    print(" ---" * 8)
 
 
-def player_1(pieces):
-    
-     print("Player 1\n where would you like the king?:")
-   # print("x: ")
-     x = int(input("x: "))
-   #  print("y: ")
-     y = int(input("y: "))
+def player_1():
+    print("Player 1\n Where would you like the white king?")
+    x = int(input("x: "))
+    y = int(input("y: "))
+    white_king = Piece("wk", x, y)
+    return white_king
 
-     white_king = pieces("wk", x, y)
-     board(white_king, white_king.position)
-     return white_king
-    
-     
-def player_2(pieces):
-    
-     print("Player 2\n where would you like the black king?:")
-   # print("x: ")
-     x = int(input("x: "))
-    # print("y: ")
-     y = int(input("y: "))
 
-     black_king = pieces("", x, y)
-     return black_king
+def player_2():
+    print("Player 2\n Where would you like the black king?")
+    x = int(input("x: "))
+    y = int(input("y: "))
+    black_king = Piece("bk", x, y)
+    return black_king
 
 
 def game():
-   while (1):
-    player_1(pieces)
-    player_2(pieces)
-   
+    white_king = player_1()
+    black_king = player_2()
+    while True:
+        board(white_king, black_king)
+        print("Player 1's turn to move the white king")
+        white_king.position = (int(input("New x: ")), int(input("New y: ")))
+        board(white_king, black_king)
+        print("Player 2's turn to move the black king")
+        black_king.position = (int(input("New x: ")), int(input("New y: ")))
+
 
 game()
